21d79b9e4b4ed1d43161819097a3b6ee6a6b6712
ryosiura <70178709+ryosiura@users.noreply.github.com>
2025-01-14 21:42:00 -0300

    Add files via upload

---
+++ pages/Dados brutos.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import requests
+import pandas as pd
+import time
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!',icon = "✅")
+    time.sleep(5)
+    sucesso.empty()
+
+
+st.title('DADOS BRUTOS')
+
+url = 'https://labdados.com/produtos'
+
+# Realizando a requisição já com o filtro solicitado
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns),list(dados.columns))
+
+st.sidebar.title('Filtros')
+
+with st.sidebar.expander('Nome do Produto'):
+    produtos = st.multiselect('Selecione os produtos',dados['Produto'].unique(),dados['Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço',0,5000,(0,5000))
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data',(dados['Data da Compra'].min(),dados['Data da Compra'].max()))
+
+query='''
+Produto in @produtos and \
+@preco[0] <= Preço <= @preco[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1]
+'''
+# @preco[0] <= Preço <= preco[1] and \
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabla possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility='collapsed')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer donwload da tabela em csv',data = converte_csv(dados_filtrados), file_name= nome_arquivo, mime='text/csv', on_click=mensagem_sucesso) 
